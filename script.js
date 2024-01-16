6cfde9fae3c5a7dac9d733ede6ec6c43ba012174
Alex Leitch <aleitch1@umd.edu>
2024-01-16 16:52:13 -0500

    cards flip properly

--- script.js
+++ script.js
@@ -1,241 +1,101 @@
-var arcoptions = ["Collapse", "Discipline", "Grow", "Transform"];
-var subtextoptions = [
-  "A few years",
-  "A decade",
-  "A generation",
-  "Two generations",
-  "A century",
-  "A millennium"
-];
-var terrainoptions = [
-  "Agriculture",
-  "The Brain",
-  "Childhood",
-  "Citizenship",
-  "Class",
-  "Climate",
-  "Cloning",
-  "Communications",
-  "Court",
-  "Disease",
-  "The Economy",
-  "Drones",
-  "The Economy",
-  "Entertainment",
-  "Environment",
-  "Equality",
-  "Family",
-  "Fashion",
-  "Flight",
-  "Forests",
-  "Genetics",
-  "Gender",
-  "Governance",
-  "Health",
-  "Hobbies",
-  "Home",
-  "Identity",
-  "Insects",
-  "Intellectual Property",
-  "Journalism",
-  "Justice",
-  "Learning",
-  "Memory",
-  "Mining",
-  "The Moon",
-  "Music",
-  "Oceans",
-  "Oil",
-  "Old Age",
-  "Pets",
-  "Power",
-  "Religion",
-  "Robots",
-  "Sex",
-  "Shopping",
-  "Space",
-  "Sports",
-  "Theatre",
-  "Travel",
-  "War",
-  "Water",
-  "Wealth",
-  "Women",
-  "Work",
-  "Zombies",
-  "The Zoo"
-];
-var objectoptions = [
-  "Advertisement",
-  "Artwork",
-  "Beverage",
-  "Book",
-  "Bottle",
-  "Box",
-  "Brochure",
-  "Building",
-  "Candy",
-  "Clothing",
-  "Corporation",
-  "Device",
-  "Document",
-  "Event",
-  "Festival",
-  "Flag",
-  "Game",
-  "Gift",
-  "Headline",
-  "Implant",
-  "Instrument",
-  "Jewellery",
-  "Kit",
-  "Law",
-  "Logo",
-  "Lotion",
-  "Machine",
-  "Magazine cover",
-  "Map",
-  "Mask",
-  "Monument",
-  "Passport",
-  "Pill",
-  "Plant",
-  "Postcard",
-  "Poster",
-  "Product",
-  "Prosthetic",
-  "Public service announcement",
-  "Relic",
-  "Ritual",
-  "Show",
-  "Slogan",
-  "Snack",
-  "Song",
-  "Souvenir",
-  "Statue",
-  "Sticker",
-  "Symbol",
-  "T-Shirt",
-  "Tattoo",
-  "Tool",
-  "Toy",
-  "Vehicle",
-  "Video",
-  "Weapon"
-];
-var moodoptions = [
-  "Admiration",
-  "Alienation",
-  "Amusement",
-  "Anger",
-  "Anxiety",
-  "Awkwardness",
-  "Calm",
-  "Charm",
-  "Cheer",
-  "Contentment",
-  "Curiosity",
-  "Decadence",
-  "Delight",
-  "Dignity",
-  "Disgust",
-  "Dread",
-  "Embarrassment",
-  "Excitement",
-  "Exhilaration",
-  "Fascination",
-  "Fervor",
-  "Frustration",
-  "Gratitude",
-  "Happiness",
-  "Hilarity",
-  "Hope",
-  "Longing",
-  "Malaise",
-  "Melancholy",
-  "Melodrama",
-  "Nostalgia",
-  "Optimism",
-  "Outrage",
-  "Pathos",
-  "Pleasure",
-  "Pride",
-  "Rationality",
-  "Relief",
-  "Resentment",
-  "Respect",
-  "Sadness",
-  "Satisfaction",
-  "Serenity",
-  "Shame",
-  "Shock",
-  "Sorrow",
-  "Surprise",
-  "Unease",
-  "Warmth",
-  "Weirdness",
-  "Wellbeing",
-  "Wonder",
-  "Worry",
-  "Zen"
-];
+function turnCard(evt){
 
-function showarc() {
-  document.getElementById("arccover").style.display = "none";
-  document.getElementById("arccard").style.display = "inline";
-  var arctodisplay = Math.floor(Math.random() * arcoptions.length);
-  var subtexttodisplay = Math.floor(Math.random() * subtextoptions.length);
-  document.getElementById("arc").innerHTML = arcoptions[arctodisplay];
-  document.getElementById("arcsubtext").innerHTML =
-    subtextoptions[subtexttodisplay];
 }
 
-function showterrain() {
-  document.getElementById("terraincover").style.display = "none";
-  document.getElementById("terraincard").style.display = "inline";
-  var terraintodisplay = Math.floor(Math.random() * terrainoptions.length);
-  document.getElementById("terrain").innerHTML =
-    terrainoptions[terraintodisplay];
+function cardActions() {
+  const cards = [...document.querySelectorAll('.card')]; // convert NodeList to array
+  const arc = document.querySelector('#arc');
+  const terrain = document.querySelector('#terrain');
+  const objt = document.querySelector('#objt');
+  const mood = document.querySelector('#mood');
+
+  cards.forEach(card => {
+    // Detect when the animation ends
+    card.addEventListener('click', (evt) => {
+        console.log(evt.target.id);
+        const clickedCard = evt.target
+        const label = clickedCard.querySelector('.label');
+        const contents = clickedCard.querySelector('.contents');
+
+        console.log(clickedCard.classList)
+        const classList = [...clickedCard.classList];
+        if(classList.includes("flipped")){
+          contents.classList.toggle("hidden");
+          clickedCard.classList.toggle("flipped");
+          setTimeout(() => {
+            label.classList.toggle("hidden");
+          }, 200);
+        } else {
+          label.classList.toggle("hidden");
+          clickedCard.classList.toggle("flipped");
+          setTimeout(() => {
+            contents.classList.toggle("hidden");
+          }, 200);
+        }
+    })
+  })
+
 }
 
-function showobject() {
-  document.getElementById("objectcover").style.display = "none";
-  document.getElementById("objectcard").style.display = "inline";
-  var objecttodisplay = Math.floor(Math.random() * objectoptions.length);
-  document.getElementById("object").innerHTML = objectoptions[objecttodisplay];
-}
+// function showarc() {
+//   document.getElementById("arccover").style.display = "none";
+//   document.getElementById("arccard").style.display = "inline";
+//   var arctodisplay = Math.floor(Math.random() * arcoptions.length);
+//   var subtexttodisplay = Math.floor(Math.random() * subtextoptions.length);
+//   document.getElementById("arc").innerHTML = arcoptions[arctodisplay];
+//   document.getElementById("arcsubtext").innerHTML =
+//     subtextoptions[subtexttodisplay];
+// }
 
-function showmood() {
-  document.getElementById("moodcover").style.display = "none";
-  document.getElementById("moodcard").style.display = "inline";
-  var moodtodisplay = Math.floor(Math.random() * moodoptions.length);
-  document.getElementById("mood").innerHTML = moodoptions[moodtodisplay];
-}
+// function showterrain() {
+//   document.getElementById("terraincover").style.display = "none";
+//   document.getElementById("terraincard").style.display = "inline";
+//   var terraintodisplay = Math.floor(Math.random() * terrainoptions.length);
+//   document.getElementById("terrain").innerHTML =
+//     terrainoptions[terraintodisplay];
+// }
 
-function fliparc() {
-  document.getElementById("arccard").style.display = "none";
-  document.getElementById("arccover").style.display = "inline";
-}
+// function showobject() {
+//   document.getElementById("objectcover").style.display = "none";
+//   document.getElementById("objectcard").style.display = "inline";
+//   var objecttodisplay = Math.floor(Math.random() * objectoptions.length);
+//   document.getElementById("object").innerHTML = objectoptions[objecttodisplay];
+// }
 
-function flipterrain() {
-  document.getElementById("terraincard").style.display = "none";
-  document.getElementById("terraincover").style.display = "inline";
-}
+// function showmood() {
+//   document.getElementById("moodcover").style.display = "none";
+//   document.getElementById("moodcard").style.display = "inline";
+//   var moodtodisplay = Math.floor(Math.random() * moodoptions.length);
+//   document.getElementById("mood").innerHTML = moodoptions[moodtodisplay];
+// }
 
-function flipobject() {
-  document.getElementById("objectcard").style.display = "none";
-  document.getElementById("objectcover").style.display = "inline";
-}
+// function fliparc() {
+//   document.getElementById("arccard").style.display = "none";
+//   document.getElementById("arccover").style.display = "inline";
+// }
 
-function flipmood() {
-  document.getElementById("moodcard").style.display = "none";
-  document.getElementById("moodcover").style.display = "inline";
-}
+// function flipterrain() {
+//   document.getElementById("terraincard").style.display = "none";
+//   document.getElementById("terraincover").style.display = "inline";
+// }
 
-function showoverlay() {
-  document.getElementById("overlay").style.display = "block";
-}
+// function flipobject() {
+//   document.getElementById("objectcard").style.display = "none";
+//   document.getElementById("objectcover").style.display = "inline";
+// }
 
-function hideoverlay() {
-  document.getElementById("overlay").style.display = "none";
-}
+// function flipmood() {
+//   document.getElementById("moodcard").style.display = "none";
+//   document.getElementById("moodcover").style.display = "inline";
+// }
+
+// function showoverlay() {
+//   document.getElementById("overlay").style.display = "block";
+// }
+
+// function hideoverlay() {
+//   document.getElementById("overlay").style.display = "none";
+// }
+
+
+document.addEventListener('DOMContentLoaded', async () => {cardActions()})
